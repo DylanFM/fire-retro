f7e2d15ed12baf377360fe4cbc41dbb6b593d521
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2015-01-07 23:05:59 +1100

    Author HTML in src and minify into dist for reload and serving.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-ruby-sass'),
+    htmlmin     = require('gulp-htmlmin'),
     browserSync = require('browser-sync'),
     reload      = browserSync.reload;
 
@@ -22,11 +23,24 @@ gulp.task('sass', function() {
     }));
 });
 
+gulp.task('htmlmin', function() {
+  gulp.src('src/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
 gulp.task('watch', function() {
   // Reload browser when built assets change
   gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], { cwd: 'dist' }, reload);
   // Build sass files on change
   gulp.watch(['styles/**/*.scss'], { cwd: 'src' }, ['sass']);
+  // Minify HTML files on change
+  gulp.watch(['*.html'], { cwd: 'src' }, ['htmlmin']);
 });
 
 gulp.task('default', ['watch', 'serve']);
