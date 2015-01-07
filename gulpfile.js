b665e6ebe369cf309591c7937ef6bdc2be500e47
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2015-01-07 22:55:48 +1100

    Setup sass.
    Authored in src and built to dist from where it is served.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp        = require('gulp')
-    browserSync = require('browser-sync');
+var gulp        = require('gulp'),
+    sass        = require('gulp-ruby-sass'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload;
 
 gulp.task('serve', function() {
   browserSync({
@@ -9,9 +11,22 @@ gulp.task('serve', function() {
   });
 });
 
+gulp.task('sass', function() {
+  gulp.src('src/styles/**/*.scss')
+    .pipe(sass({
+      style: 'expanded'
+    }))
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
 gulp.task('watch', function() {
-  // Reload browser when assets change
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], { cwd: 'dist' }, browserSync.reload);
+  // Reload browser when built assets change
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], { cwd: 'dist' }, reload);
+  // Build sass files on change
+  gulp.watch(['styles/**/*.scss'], { cwd: 'src' }, ['sass']);
 });
 
 gulp.task('default', ['watch', 'serve']);
