85054e894e728d8d6842202d11ed025323f22623
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2015-01-07 22:42:29 +1100

    A little browser sync server running.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+var gulp        = require('gulp')
+    browserSync = require('browser-sync');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    }
+  });
 });
+
+gulp.task('watch', function() {
+  // Reload browser when assets change
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], { cwd: 'dist' }, browserSync.reload);
+});
+
+gulp.task('default', ['watch', 'serve']);
