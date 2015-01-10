da7ede491cdbc678f63b3a7c07d7941350666c20
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2015-01-11 01:01:28 +1100

    Move map setup to own file.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,9 @@
-import 'mapbox.js';
+import setupMap from './setupMap';
 
 (() => {
   'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
-    var map;
-
-    // Mapbox access token
-    L.mapbox.accessToken = 'pk.eyJ1IjoiZmlyZXMiLCJhIjoiRlFmUjBYVSJ9.82br3TK-5l3LGHBfg3Yjnw';
-    // Initialise map
-    map = L.mapbox.map('map', 'fires.kng35dfj', {
-      zoomControl: false
-    });
-    // Fit to NSW
-    map.fitBounds([
-      [-37.50505999800001, 140.999474528],
-      [-28.157019914000017, 153.65]
-    ]);
+    var map = setupMap();
   });
 }());
