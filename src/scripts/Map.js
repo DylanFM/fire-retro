d7b3a04701922d6eadc774a739cecef8e8c72719
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2015-01-12 00:36:51 +1100

    Move to an ES6 class for Map

---
+++ src/scripts/Map.js
@@ -0,0 +1,27 @@
+import 'mapbox.js';
+
+export default class Map {
+
+  constructor(id) {
+    this.id = id;
+    this.accessToken = 'pk.eyJ1IjoiZmlyZXMiLCJhIjoiRlFmUjBYVSJ9.82br3TK-5l3LGHBfg3Yjnw';
+    this.mapId = 'fires.kng35dfj';
+
+    this.initMap();
+  }
+
+  initMap() {
+    // Mapbox access token
+    L.mapbox.accessToken = this.accessToken;
+    // Initialise map
+    this.map = L.mapbox.map(this.id, this.mapId, {
+      zoomControl: false
+    });
+    // Fit to NSW
+    this.map.fitBounds([
+      [-37.50505999800001, 140.999474528],
+      [-28.157019914000017, 153.65]
+    ]);
+  }
+
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
-import setupMap from './setupMap';
+import Map from './Map';
 import d3 from 'd3';
 
 (() => {
   'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
-    var map = setupMap();
+    // Construct new map. Pass in the ID of the DOM element
+    var map = new Map('map');
 
     d3.json('http://localhost:8000/incidents?timeStart=2014-01-24T00:00:00Z&timeEnd=2014-01-24T23:59:59Z', (json) => {
       L.geoJson(json, {
@@ -15,7 +16,7 @@ import d3 from 'd3';
             fillOpacity:  0.5
           });
         }
-      }).addTo(map);
+      }).addTo(map.map);
     });
   });
 }());
